a048412402f4d6c8012a19dbd145b2a4c60c22f2
邵则开 <8988546+shao-zekai@user.noreply.gitee.com>
2021-08-21 13:19:19 +0000

    sentence tsne completed

---
+++ ABCview/vue_test/src/components/ThresholdSelected.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="threshold-selected">
+    <a-slider :marks='marks' :max=0.5 :min=0.1 :step=0.1 :default-value="0.4" 
+      @afterChange="onAfterChange" 
+              />
+  </div>
+</template>
+
+<script>
+import bus from './bus'
+
+export default {
+  name: 'ThresholdSelected',
+  methods: {
+     onAfterChange(value) {
+       console.log('after change')
+      this.threshold = value;
+      bus.$emit("dispatchthreshold",this.threshold);
+    },
+    
+  },
+  data(){
+    return {
+      marks: {
+        0.1: '0.1',
+        0.2: '0.2',
+        0.3: '0.3',
+        0.4: '0.4',
+        0.5: '0.5',
+        },
+      threshold:0.4
+    }
+  }
+}
+</script>
+
+<style>
+</style>
